da811aac6c9a2bfe16c3d6df4cfa257760a5b95c
Gerben Neven <gerbyzation@me.com>
2015-07-19 21:00:51 +0200

    use wait_for function for URL checks in my_lists

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -21,7 +21,6 @@ class MyListsTest(FunctionalTest):
             path='/',
         ))
 
-
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         # Edith is a logged-in user
         self.create_pre_authenticated_session('edith@example.com')
@@ -38,7 +37,9 @@ class MyListsTest(FunctionalTest):
         # She sees that her list is in there, named according to its
         # first list item
         self.browser.find_element_by_link_text('Reticulate splines').click()
-        self.assertEqual(self.browser.current_url, first_list_url)
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, first_list_url)
+        )
 
         # She decides to start another list, just to see
         self.browser.get(self.server_url)
